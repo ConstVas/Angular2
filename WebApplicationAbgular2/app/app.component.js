ae092a072e1c8fe8d9c24b51e64f89fa2368745a
VasilevKA <VasilevKA@quarta.su>
2017-02-06 14:15:51 +0300

    now working well

--- WebApplicationAbgular2/app/app.component.js
+++ WebApplicationAbgular2/app/app.component.js
@@ -14,9 +14,10 @@ var AppComponent = (function () {
 }());
 AppComponent = __decorate([
     core_1.Component({
+        moduleId: module.id,
         selector: 'my-app',
-        template: "\n  <h1>{{title}}</h1>\n  <nav>\n    <a routerLink=\"/dashboard\" routerLinkActive=\"active\">Dashboard</a>\n    <a routerLink=\"/heroes\" routerLinkActive=\"active\">Heroes</a>\n  </nav>\n  <router-outlet></router-outlet>\n",
-        styleUrls: ['./app.component.css'],
+        templateUrl: './app.component.html',
+        styleUrls: ['./app.component.css']
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
